f86a77c3644973457139f8536cf25ca327a56972
muriloribeir <murilo.santos230@etec.sp.gov.br>
2024-06-04 20:20:40 -0300

    uga

--- desafio.c
+++ desafio.c
@@ -1,47 +1,62 @@
 #include <stdio.h>
 
+void soma (float valor1, float valor2) {
+	float resultado;
+	resultado = valor1 + valor2;
+	printf("Resultado da soma:%.2f\n");
+}
+
+void subtracao (float valor1, float valor2) {
+	float resultado;
+	resultado = valor1 - valor2;
+	printf("Resultado da subtracao:%.2f\n");
+}
+
+void multiplicacao (float valor1, float valor2) {
+	float resultado;
+	resultado = valor1 * valor2;
+	printf("Resultado da multiplicacao:%.2f\n",resultado);
+}
+
+void divisao (float valor1, float valor2) {
+	float resultado;
+	resultado = valor2 / valor1;
+	printf("Resultado da divisao:%.2f\n",resultado);
+}
+
+float pegaValor(){
+	float valor;
+	printf ("Digite um valor:\n");
+	return valor;
+	
+}
+
 void main() {
     char operacao;
-    float resultado, num1, num2;
-    int cont;
-    
-    cont= 1;
-while(cont){
-    printf("Digite o primeiro numero: ");
-    scanf("%f", &num1);
+  
     printf("Escolha a operacao (+, -, *, /): ");
     scanf(" %c", &operacao);
-    printf("Digite o segundo numero: ");
-    scanf("%f", &num2);
 
     switch(operacao) {
         case '+':
-            resultado = num1 + num2;
-            printf("Resultado: %.2f\n", resultado);
+            soma(pegaValor(),pegaValor());
             break;
         case '-':
-            resultado = num1 - num2;
-            printf("Resultado: %.2f\n", resultado);
+            subtracao(pegaValor(),pegaValor());
             break;
         case '*':
-            resultado = num1 * num2;
-            printf("Resultado: %.2f\n", resultado);
+            multiplicacao(pegaValor(),pegaValor());
             break;
         case '/':
-            if (num2 != 0) {
-                resultado = num1 / num2;
-                printf("Resultado: %.2f\n", resultado);
-            } else {
+            if (pegaValor() !=0 && pegaValor() !=0) {
+                divisao(pegaValor(),pegaValor());
+            } 
+	    else {
                 printf("Erro: Não pode conter número 0\n");
-            }
-            break;
-        default:
-            printf("Operacao invalida\n");
-    }
-    printf ("Deseja continuar? \ndigite 1 para SIM e 0 para NÃO ");
-    scanf("%d",&cont);
-  }
-}
+            }	
+	    break;
+
+}} 
 
 
 
